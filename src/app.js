9d5fd5c1be24a44fef80a157e6626037d114cf54
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2018-11-29 13:03:45 +0100

    Still on CORS fixing

--- src/app.js
+++ src/app.js
@@ -11,10 +11,8 @@ const app = express();
 
 // fixing CORS
 app.use((req, res, next) => {
-  // setting the headers
-res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'POST, PATCH, DELETE, GET');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
